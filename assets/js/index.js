e387a1a61276ab7feeeb5d1461b8de7694234f46
“romaindeschambres” <romain.deschambres@gmail.com>
2018-06-15 17:52:50 +0200

    integration partie 1 fix bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,18 @@
 		});
 	});
 })(jQuery);
+
+
+function fileValidation(){
+    var fileInput = document.getElementById('televerser');
+    var filePath = fileInput.value;
+    var allowedExtensions = /(\.pdf|\.doc|\.zip)$/i;
+    if(!allowedExtensions.exec(filePath)){
+        document.getElementById("error-message").classList.add("error-message-visible");
+        fileInput.value = '';
+        return false;
+    }else{
+        document.getElementById("error-message").classList.remove("error-message-visible");
+        return true;
+    }
+}
